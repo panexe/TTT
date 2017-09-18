a2fc4b2b057641c3136b736961d617a52dd774d2
panexe <larslpt@gmail.com>
2017-09-18 09:32:48 +0200

    first commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bnt_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_Optionen = new System.Windows.Forms.Button();
+            this.btn_newGame = new System.Windows.Forms.Button();
+            this.label_züge = new System.Windows.Forms.Label();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bnt_1
+            // 
+            this.bnt_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bnt_1.Location = new System.Drawing.Point(12, 96);
+            this.bnt_1.Name = "bnt_1";
+            this.bnt_1.Size = new System.Drawing.Size(75, 75);
+            this.bnt_1.TabIndex = 0;
+            this.bnt_1.TabStop = false;
+            this.bnt_1.UseVisualStyleBackColor = true;
+            this.bnt_1.Click += new System.EventHandler(this.btn_Press);
+            // 
+            // btn_2
+            // 
+            this.btn_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_2.Location = new System.Drawing.Point(93, 96);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(75, 75);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.TabStop = false;
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_Press);
+            // 
+            // btn_3
+            // 
+            this.btn_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_3.Location = new System.Drawing.Point(174, 96);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(75, 75);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.TabStop = false;
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_Press);
+            // 
+            // btn_4
+            // 
+            this.btn_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_4.Location = new System.Drawing.Point(12, 177);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(75, 75);
+            this.btn_4.TabIndex = 3;
+            this.btn_4.TabStop = false;
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_Press);
+            // 
+            // btn_5
+            // 
+            this.btn_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_5.Location = new System.Drawing.Point(93, 177);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(75, 75);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.TabStop = false;
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_Press);
+            // 
+            // btn_6
+            // 
+            this.btn_6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_6.Location = new System.Drawing.Point(174, 177);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(75, 75);
+            this.btn_6.TabIndex = 5;
+            this.btn_6.TabStop = false;
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_Press);
+            // 
+            // btn_7
+            // 
+            this.btn_7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_7.Location = new System.Drawing.Point(12, 258);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(75, 75);
+            this.btn_7.TabIndex = 6;
+            this.btn_7.TabStop = false;
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.btn_Press);
+            // 
+            // btn_8
+            // 
+            this.btn_8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_8.Location = new System.Drawing.Point(93, 258);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(75, 75);
+            this.btn_8.TabIndex = 7;
+            this.btn_8.TabStop = false;
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.btn_Press);
+            // 
+            // btn_9
+            // 
+            this.btn_9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_9.Location = new System.Drawing.Point(174, 258);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(75, 75);
+            this.btn_9.TabIndex = 8;
+            this.btn_9.TabStop = false;
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.btn_Press);
+            // 
+            // btn_Optionen
+            // 
+            this.btn_Optionen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Optionen.Location = new System.Drawing.Point(93, 12);
+            this.btn_Optionen.Name = "btn_Optionen";
+            this.btn_Optionen.Size = new System.Drawing.Size(162, 37);
+            this.btn_Optionen.TabIndex = 9;
+            this.btn_Optionen.TabStop = false;
+            this.btn_Optionen.Text = "Optionen";
+            this.btn_Optionen.UseVisualStyleBackColor = true;
+            this.btn_Optionen.Click += new System.EventHandler(this.btn_optionen);
+            // 
+            // btn_newGame
+            // 
+            this.btn_newGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_newGame.Location = new System.Drawing.Point(12, 12);
+            this.btn_newGame.Name = "btn_newGame";
+            this.btn_newGame.Size = new System.Drawing.Size(75, 37);
+            this.btn_newGame.TabIndex = 10;
+            this.btn_newGame.TabStop = false;
+            this.btn_newGame.Text = "Neues Spiel";
+            this.btn_newGame.UseVisualStyleBackColor = true;
+            this.btn_newGame.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label_züge
+            // 
+            this.label_züge.AutoSize = true;
+            this.label_züge.Location = new System.Drawing.Point(334, 127);
+            this.label_züge.Name = "label_züge";
+            this.label_züge.Size = new System.Drawing.Size(38, 13);
+            this.label_züge.TabIndex = 11;
+            this.label_züge.Text = "Züge :";
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.Location = new System.Drawing.Point(334, 96);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(28, 13);
+            this.lbl_time.TabIndex = 12;
+            this.lbl_time.Text = "Zeit:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 542);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.label_züge);
+            this.Controls.Add(this.btn_newGame);
+            this.Controls.Add(this.btn_Optionen);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.bnt_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnt_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_Optionen;
+        private System.Windows.Forms.Button btn_newGame;
+        private System.Windows.Forms.Label label_züge;
+        private System.Windows.Forms.Label lbl_time;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        bool Turn;
+        public Form1()
+        {
+            InitializeComponent();
+            Turn = false;
+        }
+
+        public void btn_Press(object sender ,EventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Enabled = false;
+            if(Turn)
+            b.Text = "X";
+            else { b.Text = "O"; }
+            Turn = !Turn;
+              
+        }
+
+        private void btn_optionen(object sender, EventArgs e)
+        {
+            Form_Optionen optionen = new Form_Optionen();
+            optionen.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
